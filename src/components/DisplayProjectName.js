5454bc4923f8081a8c60ede6ccdfcb49fae77ff6
Paul Olivier AHOUA <ahouapaulolivier@gmail.com>
2021-06-08 06:28:24 +0200

    Add button to copy project token

--- src/components/DisplayProjectName.js
+++ src/components/DisplayProjectName.js
@@ -1,21 +1,38 @@
 import React, {Fragment, useEffect, useState} from "react";
 import {useRecoilValue} from "recoil";
 import {ProjectAtom} from "../atoms/ProjectAtom";
+import ShareIcon from '@material-ui/icons/Share';
 
 const DisplayProjectName = () => {
 
     const projectString = useRecoilValue(ProjectAtom);
     let project = "N/D";
+    let projectToken = "";
 
-    if(projectString != ""){
+    if (projectString != "") {
         const projectObject = JSON.parse(projectString);
         project = projectObject.name;
+        projectToken = projectObject.token;
+    }
+
+    const handleShareClick = () => {
+        navigator.clipboard.writeText(projectToken).then(() => {
+            alert("Token copied to clipboard");
+        })
     }
 
     return (
         <Fragment>
             <div className={'project-name-container'}>
-                <p>Current project : <span>{project}</span></p>
+
+                <p>
+                    Current project: &nbsp;
+                    <span>{project}</span>
+                    {
+                        projectToken !== "" &&
+                        <span className={'share'} onClick={handleShareClick}> | Share</span>
+                    }
+                </p>
             </div>
             <style jsx>{`
                 .project-name-container {
@@ -24,6 +41,8 @@ const DisplayProjectName = () => {
                     top: 0;
                     padding: 0 10px;
                     z-index: 20;
+                    display: flex;
+                    flex-direction: row;
                 }
                 
                 .project-name-container p {
@@ -34,6 +53,11 @@ const DisplayProjectName = () => {
                 
                 .project-name-container p span {
                     font-weight: bold;
+                }
+                 
+                .share {
+                    font-size: 12px;
+                    cursor: pointer
                 }
             `}</style>
         </Fragment>
