ea4cc94325a9310c253cb0442d43325373479f68
Paul Olivier AHOUA <ahouapaulolivier@gmail.com>
2021-06-08 02:39:33 +0200

    Set new project data

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -2,22 +2,48 @@ import React, {Fragment, useState} from "react";
 import {useRecoilState, useSetRecoilState} from "recoil";
 import {CodeAtom} from "../atoms/CodeAtom";
 import {ProjectAtom} from "../atoms/ProjectAtom";
+import {createSnippet} from "../services/Snippets";
+import useToken from "./useToken";
+import randomWord from 'random-words';
 
 const Menu = ({setNewProjectDialogOpen}) => {
 
-    const setProject = useSetRecoilState(ProjectAtom);
+    const [project, setProject] = useRecoilState(ProjectAtom);
     const [code, setCode] = useRecoilState(CodeAtom);
     const counterBlock = code.length;
+    const {token, setToken} = useToken();
 
-    const handleAddBlock = () => {
+    const handleAddBlock = (snippetData) => {
         //Adding new block
         setCode((oldCode) => [
             ...oldCode,
-            {
-                id: counterBlock + 1,
-                codeValue: ""
-            }
+            snippetData
         ]);
+    }
+
+    const handleNewSnippet = async () => {
+        if (project == "") {
+            setNewProjectDialogOpen(true);
+        } else {
+            const projectObject = JSON.parse(project);
+            const snippetName = randomWord();
+            const codeValue = "";
+            const response = await createSnippet(token, snippetName, codeValue, projectObject.id);
+
+            if(response.error) {
+                console.log("error");
+                return;
+            }
+
+            const snippetData = {
+                id: counterBlock + 1,
+                name: snippetName,
+                codeValue: "",
+                projectID: projectObject.id
+            }
+
+            handleAddBlock(snippetData);
+        }
     }
 
     const handleCloseProject = () => {
@@ -28,7 +54,7 @@ const Menu = ({setNewProjectDialogOpen}) => {
         <Fragment>
             <ul className={'menu-container'}>
                 <li onClick={() => setNewProjectDialogOpen(true)}>New project (CMD+N)</li>
-                <li onClick={handleAddBlock}>New snippet (CMD+N+S)</li>
+                <li onClick={handleNewSnippet}>New snippet (CMD+N+S)</li>
                 <li>Open project (CMD+O)</li>
                 <li onClick={handleCloseProject}>Close project (CMD+W)</li>
                 <li>Delete project (CMD+X)</li>
