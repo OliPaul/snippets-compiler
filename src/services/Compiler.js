97824b88693f2035f8a6025522fb780087871e2c
Paul Olivier AHOUA <ahouapaulolivier@gmail.com>
2021-07-07 10:37:47 +0200

    Api to compilation route

--- src/services/Compiler.js
+++ src/services/Compiler.js
@@ -1,6 +1,6 @@
 const compile = async (token, snippetsId, projectId, language = "c") => {
 
-    const url = process.env.REACT_APP_BACK_URL + "/compiler";
+    const url = process.env.REACT_APP_BACK_URL + "/api/compiler";
 
     const compilationData = {
         snippetsId: snippetsId,
