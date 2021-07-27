df71e5541f45c7f704cd21d4eb615690ebc600b4
sonia2896 <zemsonia@hotmail.com>
2021-07-27 15:14:39 +0200

    Implement websocket

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import Auth from "./pages/auth";
 import Content from "./pages/content";
 import useToken from "./components/useToken";
 import DisplayProjectName from "./components/DisplayProjectName";
-import SockJsClient from 'react-stomp';
 
 
 function App() {
@@ -15,28 +14,11 @@ function App() {
         return <Auth setToken={setToken} />
     }
 
-    let SOCKET_URL = 'http://localhost:8080/snippets';
-
-    function onMessageReceived(msg) {
-        //Cette fonction est appellé a chaque fois qu'il y a un changement sur les snippets du projet associé
-        console.log(msg);
-    }
-
     return (
         <RecoilRoot>
             <DisplayProjectName />
             <Header setToken={setToken} />
             <Content />
-            <div>
-                <SockJsClient
-                    url={SOCKET_URL}
-                    topics={['/listener/projects/3']} // Dans l'url il faut mettre l'id du projet
-                    onConnect={console.log("Connected!")} // Pas vraiment utile
-                    onDisconnect={console.log("Disconnected!")}// Pas vraiment utile 
-                    onMessage={msg => onMessageReceived(msg)}
-                    debug={false}
-                />
-            </div>
         </RecoilRoot>
 
     );
