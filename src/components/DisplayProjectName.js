eb4e2e94d95686e298cff1801a9eeaa895b0dffc
JBertho <james.bertho94@gmail.com>
2021-07-28 21:55:35 +0200

    Change clipboard access

--- src/components/DisplayProjectName.js
+++ src/components/DisplayProjectName.js
@@ -16,8 +16,20 @@ const DisplayProjectName = () => {
     }
 
     const handleShareClick = () => {
-        navigator.clipboard.writeText(projectToken).then(() => {
+        let textArea = document.createElement("textarea");
+        textArea.value = projectToken;
+        // make the textarea out of viewport
+        textArea.style.position = "fixed";
+        textArea.style.left = "-999999px";
+        textArea.style.top = "-999999px";
+        document.body.appendChild(textArea);
+        textArea.focus();
+        textArea.select();
+        return new Promise((res, rej) => {
+            // here the magic happens
+            document.execCommand('copy') ? res() : rej();
             alert("Token copied to clipboard");
+            textArea.remove();
         })
     }
 
